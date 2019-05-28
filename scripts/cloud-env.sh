68f51d7e10cbeea2f8fbadc15809d58ec64d5f95
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2019-05-28 11:40:27 +0200

    Added cloud-env.sh.
    This file contains the name bash-variables for quick access to various
    aspects of the Google Cloud.

---
+++ scripts/cloud-env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+export PROJECT=nomadic-armor-240011
+export BUCKET=dataproc-5c73e9da-4bc9-4b31-bd76-a4c6b6d5741b-europe-north1
+export CLUSTER=spark
+export ZONE=europe-north1-a
