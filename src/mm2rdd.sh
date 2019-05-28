a2ff0cef289a882cbaf1f24c3626387a6c6f14ad
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2019-05-28 13:13:13 +0200

    Batch-submission script for MatrixMarket to RDD.

---
+++ src/mm2rdd.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# Convert all MatrixMarket files to RDDs on our Google bucket.
+
+top=$(git rev-parse --show-toplevel)
+
+for item in $(gsutil ls gs://${BUCKET}/input/mm/*.mtx); do
+    printf "Processing: %s\n" ${item}
+    file=$(basename ${item} .mtx)
+    gcloud dataproc jobs submit pyspark \
+           ${top}/src/matrix_rdd.py \
+           --cluster=${CLUSTER} \
+           -- \
+           ${item} \
+           gs://${BUCKET_NAME}/output/mm/${file}
+done
