f9b0cf2dab077127b86e879c30bed9979957fa05
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2019-05-28 11:28:35 +0200

    Fixes in wordcount.py.
    - Proper python style.
    - Make sure that we can overwrite existing output files.

--- tutorials/wordcount.py
+++ tutorials/wordcount.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import pyspark
 import sys
 
 if len(sys.argv) != 3:
-  raise Exception("Exactly 2 arguments are required: <inputUri> <outputUri>")
+    raise Exception("Exactly 2 arguments are required: <inputUri> <outputUri>")
 
-input_uri=sys.argv[1]
-output_uri=sys.argv[2]
-
-sc = pyspark.SparkContext()
+input_uri = sys.argv[1]
+output_uri = sys.argv[2]
+conf = pyspark.SparkConf().set("spark.hadoop.validateOutputSpecs", "false")
+sc = pyspark.SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[1])
 words = lines.flatMap(lambda line: line.split())
 sum_lambda = lambda count1, count2: count1 + count2
