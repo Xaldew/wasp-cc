7615e0b247c15b70832cf8150c9644b3f0d6d204
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2019-05-28 15:56:45 +0200

    Compute SVD in the cloud.

---
+++ src/rm2svd.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Compute the singular values on all matrices.
+
+top=$(git rev-parse --show-toplevel)
+
+for item in \
+    $(gsutil ls -d gs://${BUCKET}/output/mm/10_*) \
+        $(gsutil ls -d gs://${BUCKET}/output/mm/100_*) \
+        $(gsutil ls -d gs://${BUCKET}/output/mm/1000_*) \
+    ;
+do
+    printf "Processing: %s\n" ${item}
+    svd=$(basename ${item})
+    gcloud dataproc jobs submit pyspark \
+           ${top}/src/spark_svd.py \
+           --cluster=${CLUSTER} \
+           -- \
+           ${item}/* \
+           gs://${BUCKET_NAME}/output/svd/${svd}
+done
---
+++ src/spark_svd.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import sys
+import locale
+import argparse
+import pyspark
+import pyspark.sql
+import pyspark.mllib.linalg
+import pyspark.mllib.linalg.distributed
+
+
+def main(args):
+    """Compute the Singular Value Decomposition (SVD) of a matrix.
+
+    .. Keyword Arguments:
+    :param args: The program arguments.
+
+    .. Types:
+    :type args: A argparse namespace ojects containing the program arguments.
+
+    .. Returns:
+    :returns: 0 If the program ran successfully, otherwise non-zero.
+    :rtype: An integer.
+
+    """
+    conf = pyspark.SparkConf().set("spark.hadoop.validateOutputSpecs", "false")
+    sc = pyspark.SparkContext(conf=conf)
+
+    # Required to get access to the `toDF()` function inside of CoordinateMatrix.
+    spark = pyspark.sql.SparkSession(sc)
+
+    # Re-create the row-matrix.
+    rows = sc.pickleFile(args.rows)
+    rm = pyspark.mllib.linalg.distributed.RowMatrix(rows)
+
+    # Compute the SVD, store the singular values in the output.
+    k = min(100, rm.numCols())
+    svd = rm.computeSVD(k)
+    sc.parallelize(svd.s).saveAsTextFile(args.S)
+
+    return 0
+
+
+def parse_arguments(argv):
+    """Parse the given argument vector.
+
+    .. Keyword Arguments:
+    :param argv: The arguments to be parsed.
+
+    .. Types:
+    :type argv: A list of strings.
+
+    .. Returns:
+    :returns: The parsed arguments.
+    :rtype: A argparse namespace object.
+
+    """
+    fmtr = argparse.RawDescriptionHelpFormatter
+    kdesc = "Python Spark SVD script."
+    parser = argparse.ArgumentParser(description=kdesc, formatter_class=fmtr)
+    parser.add_argument("rows", metavar="RDD", type=str,
+                        help="The matrix to read.")
+    parser.add_argument("S", metavar="RDD", type=str,
+                        help="Output RDD continaing the singular values.")
+    return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+    locale.setlocale(locale.LC_ALL, "")
+    ARGS = parse_arguments(sys.argv[1:])
+    sys.exit(main(ARGS))
