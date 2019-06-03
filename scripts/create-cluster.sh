50ae9743c5d62de388ca1f5b06f28da488aa67d3
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2019-06-03 12:57:15 +0200

    Create cluster script working.
    Can now select machine types and number of workers.

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env sh
 # Script to programatically create a cluster with python packages.
 
-gcloud dataproc clusters create pycluster \
+workers=${1:?"No workers given. Must be at least 2."}
+cores=${2-1}
+name=${3:-"spark"}
+machine="n1-standard-${cores}"
+
+gcloud dataproc clusters create ${name} \
        --image-version 1.2 \
+       --num-masters=1 \
+       --master-machine-type=${machine} \
+       --num-workers=${workers} \
+       --worker-machine-type=${machine} \
+       --region=europe-north1 \
+       --zone=europe-north1-a \
        --metadata 'PIP_PACKAGES=pandas==0.23.0 scipy==1.1.0' \
        --initialization-actions \
        gs://dataproc-initialization-actions/python/pip-install.sh
